d29b6cb99034981a8dcd5bf6c85e9e92767d9ec6
SAIEED12 <abusaieed0@gmail.com>
2026-09-19 15:45:08 +0600

    Add CORS support and JSON parsing middleware

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = process.env.port || 5000;
 
 const client = new MongoClient(uri, {
@@ -18,14 +21,27 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("dolna_db");
+    const productsCollection = db.collection("products");
+
+    //Add Products API
+    app.post('/add-products', async (req, res) => {
+      try {
+        const product = req.body;
+        const result = await productsCollection.insertOne(product);
+        res.status(201).json(result);
+      } catch (error) {
+        console.error("Error adding product:", error);
+        res.status(500).json({ error: "Failed to add product" });
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
